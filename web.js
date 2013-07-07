2abdd81d3c27e5d790e7c233a460aab1431cfc99
Ke Wang <kewang55@gmail.com>
2013-07-07 18:52:56 +0000

    Modified web.js to add index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var output_string = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(output_string);
 });
 
 var port = process.env.PORT || 5000;
