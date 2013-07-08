7a849f2222b850b6ea17ae330ae22f3cce5803ab
Ke Wang <kewang55@gmail.com>
2013-07-08 02:57:14 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -5,6 +5,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
+var rest = require('restler');
+var URL_DEFAULT = 'http://whispering-island-8025.herokuapp.com';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,15 @@ var checkHtmlFile = function(htmlfile, checksfile){
     return out;
 };
 
+
+
+var checkUrl = function(url, checksfile) {
+    rest.get(url).on('complete',function(result){ fs.writeFileSync('url.html',result); });
+    return checkHtmlFile('url.html',checksfile);
+};
+
+
+
 var clone = function(fn) {
     return fn.bind({});
 };
@@ -42,8 +53,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to page')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if(program.url != null) {
+	checkJson = checkUrl(program.url, program.checks);
+    } else {
+	checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
